49b6f733bf442b82f441e715d342cc48d68f18ab
Artem Baikov <ivankolga25@gmail.com>
2023-10-29 21:56:10 +0300

    Add user and project seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,27 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Tgate.Repo.insert!(%Tgate.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule Tgate.Seeds do
+  alias Tgate.Accounts
+  alias Tgate.Projects
+
+  def seed! do
+    user = seed_user!()
+
+    seed_project!(user)
+    :ok
+  end
+
+  defp seed_user! do
+    {:ok, user} = Accounts.register_user(%{email: "test@gmail.com", password: "123456"})
+    user
+  end
+
+  defp seed_project!(user) do
+    {:ok, project} =
+      user
+      |> Projects.exchange_for_owner()
+      |> Projects.create_project(%{name: "SpawnFest 2023 Project"})
+
+    project
+  end
+end
+
+Tgate.Seeds.seed!()
